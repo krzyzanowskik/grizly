6e37a6e014c8beaf5874e0fc221d02fc3b8a9210
Michal Zawadzki <michalmzawadzki@gmail.com>
2019-09-18 16:49:37 +0200

    fixed DELETE FROM statement in s3_to_rds_qf()

--- grizly/io/etl.py
+++ grizly/io/etl.py
@@ -146,7 +146,7 @@ def s3_to_csv(csv_path):
 
 
 
-def df_to_s3(df, table_name, schema, dtype=None, sep='\t', engine=None, delete_first=False, clean_df=False, if_exists="fail", keep_csv=False):
+def df_to_s3(df, table_name, schema, dtype=None, sep='\t', engine=None, delete_first=False, clean_df=False, keep_csv=False):
 
     """Copies a dataframe inside a Redshift schema.table
         using the bulk upload via this process:
@@ -189,16 +189,6 @@ def df_to_s3(df, table_name, schema, dtype=None, sep='\t', engine=None, delete_f
 
     bucket.upload_file(filepath, f"bulk/{filename}")
     print(f'bulk/{filename} file uploaded to s3')
-
-    if if_exists == "fail":
-        raise ValueError("Table already exists. Use if_exists='drop' or 'append' to override the table.")
-    elif if_exists == "drop":
-        engine.execute(f"DROP TABLE IF EXISTS {schema}.{table_name}")
-        df.head(1).to_sql(table_name, schema=schema, index=False, con=engine, dtype=dtype)
-    elif if_exists == "append":
-        pass
-    else:
-        raise ValueError("Please choose what to do in case the table exists. One of: drop/append.")
 
     if not keep_csv:
         os.remove(filepath)
@@ -306,7 +296,7 @@ def s3_to_rds_qf(qf, table, s3_name, schema='', if_exists='fail', sep='\t', use_
         if if_exists == 'fail':
             raise ValueError("Table {} already exists".format(table_name))
         elif if_exists == 'replace':
-            sql ="DELETE FROM {}".format(table_name)
+            sql = f"DELETE FROM {schema}.{table}"
             engine.execute(sql)
             print('SQL table has been cleaned up successfully.')
         else:
