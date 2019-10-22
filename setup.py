cd9b3de25d25f77d518e8d7a0f184653c51fbda0
malinaa96 <52569986+malinaa96@users.noreply.github.com>
2019-10-22 18:46:08 +0200

    Added requires list to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,22 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+
+requires = [
+    "boto3==1.9.200",
+    "ipython==7.6.1",
+    "openpyxl",
+    "pandas==0.25.1",
+    "SQLAlchemy==1.3.5",
+    "sqlparse==0.3.0",
+    "simple-salesforce==0.74.2",
+    "dask==2.1.0",
+    "exchangelib==2.0.1",
+    "pendulum==2.0.5",
+    "croniter==0.3.30",
+    "graphviz"
+]
+
 setuptools.setup(
     name="grizly",
     version="0.0.1",
@@ -17,4 +33,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=requires
 )
