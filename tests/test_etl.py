9ef409573d981064a6123c434c96d40c15747048
malinaa96 <52569986+malinaa96@users.noreply.github.com>
2019-12-03 16:41:00 +0100

    Fixing bugs

--- tests/test_etl.py
+++ tests/test_etl.py
@@ -1,7 +1,13 @@
-from ..grizly.etl import build_copy_statement
-
+from grizly.etl import build_copy_statement
+from configparser import ConfigParser
+from grizly.utils import get_path
 
 def test_build_copy_statement():
+
+    config = ConfigParser()
+    config.read(get_path('.aws','credentials'))
+    akey = config['default']['aws_access_key_id']
+    skey = config['default']['aws_secret_access_key']
     
     def test_time_format():
         params = {
@@ -10,7 +16,7 @@ def test_build_copy_statement():
             "table_name": "test_table", 
             "time_format": "some_time_format"
         }
-        assert build_copy_statement(**params) == """\n        COPY z_sandbox_mz.test_table FROM \'s3://teis-data/bulk/test.csv\'\n        access_key_id \'"akey"\'\n        secret_access_key \'"skey"\'\n        delimiter \'\t\'\n        NULL \'\'\n        IGNOREHEADER 1\n        REMOVEQUOTES\n        timeformat \'some_time_format\'\n        ;commit;\n        """
+        assert build_copy_statement(**params) == f"""\n        COPY z_sandbox_mz.test_table FROM \'s3://teis-data/bulk/test.csv\'\n        access_key_id \'{akey}\'\n        secret_access_key \'{skey}\'\n        delimiter \'\t\'\n        NULL \'\'\n        IGNOREHEADER 1\n        REMOVEQUOTES\n        timeformat \'some_time_format\'\n        ;commit;\n        """
     
     def test_remove_inside_quotes():
         params = {
@@ -19,7 +25,7 @@ def test_build_copy_statement():
             "table_name": "test_table", 
             "remove_inside_quotes": True
         }
-        assert build_copy_statement(**params) == """\n        COPY z_sandbox_mz.test_table FROM \'s3://teis-data/bulk/test.csv\'\n        access_key_id \'"akey"\'\n        secret_access_key \'"skey"\'\n        delimiter \'\t\'\n        NULL \'\'\n        IGNOREHEADER 1\n        CSV QUOTE AS \'\\"\'\n        ;commit;\n        """
+        assert build_copy_statement(**params) == f"""\n        COPY z_sandbox_mz.test_table FROM \'s3://teis-data/bulk/test.csv\'\n        access_key_id \'{akey}\'\n        secret_access_key \'{skey}\'\n        delimiter \'\t\'\n        NULL \'\'\n        IGNOREHEADER 1\n        CSV QUOTE AS \'\\"\'\n        ;commit;\n        """
     
     test_time_format()
     test_remove_inside_quotes()
