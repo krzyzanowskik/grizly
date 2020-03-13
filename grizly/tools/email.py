b7b3f4a0cc6b5c81aa13548d0a1cae9c591abe87
Marek <piotr.marek@te.com>
2020-03-13 10:12:42 +0100

    0.3 release_test fixes

--- grizly/tools/email.py
+++ grizly/tools/email.py
@@ -39,8 +39,7 @@ class EmailAccount:
             credentials=self.credentials,
             retry_policy=FaultTolerance(max_wait=2 * 60),
         )
-        # self.proxy = proxy or os.getenv("HTTPS_PROXY")
-        self.proxy=None
+        self.proxy = proxy or os.getenv("HTTPS_PROXY")
         if self.proxy:
             os.environ["HTTPS_PROXY"] = self.proxy
         try:
