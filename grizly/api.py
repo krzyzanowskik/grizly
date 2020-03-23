64a67d2eb6c5d24f679545af1d0392172205e7d7
acivitillo <acivitillo@te.com>
2020-03-23 05:44:03 -0400

    added orchestrate.Runner

--- grizly/api.py
+++ grizly/api.py
@@ -12,7 +12,7 @@ from .tools.sfdc import SFDC
 from .tools.s3 import S3, s3_to_csv, csv_to_s3, df_to_s3, s3_to_rds
 from .tools.github import GitHub
 from .tools.sqldb import SQLDB, check_if_exists, delete_where, get_columns, copy_table
-from .scheduling.orchestrate import Workflow, Listener, EmailListener, Schedule, retry
+from .scheduling.orchestrate import Workflow, Listener, EmailListener, Schedule, Runner, retry
 
 
 from os import environ
